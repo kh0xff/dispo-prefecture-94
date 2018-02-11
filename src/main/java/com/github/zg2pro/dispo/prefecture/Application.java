ae9b7889a17a05773f154f0c737694b2f14ada2c
Gregory Anne <englishblazere@free.fr>
2018-02-11 22:31:34 +0100

    finished, it should help

--- src/main/java/com/github/zg2pro/dispo/prefecture/Application.java
+++ src/main/java/com/github/zg2pro/dispo/prefecture/Application.java
@@ -1,48 +1,18 @@
 package com.github.zg2pro.dispo.prefecture;
 
-import com.github.zg2pro.spring.rest.basis.template.Zg2proRestTemplate;
-import java.util.Collections;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  *
  * @author zg2pro
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
-    @Bean
-    public Boolean checkAvailibility() {
-        Zg2proRestTemplate restTemplate = new Zg2proRestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XHTML_XML));
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        ResponseEntity<String> resp = restTemplate.getForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/0", String.class);
-        List<String> lStr = resp.getHeaders().get("Set-Cookie");
-        String cookie = lStr.get(0);
-        cookie = cookie.replaceAll("path=/", "");
-        cookie += "xtvrn=$481980$; xtan481980=-; xtant481980=1";
-        headers.set("Cookie", cookie);
-        //Cookie:eZSESSID=9lp2b5rfmpn7eus77h094qu9b1; xtvrn=$481980$; xtan481980=-; xtant481980=1
-        HttpEntity<String> entity = new HttpEntity<>("condition=on&nextButton=Effectuer+une+demande+de+rendez-vous", headers);
-        resp = restTemplate.postForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/0", entity, String.class);
-        resp = restTemplate.getForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/1", String.class);
-        entity= new HttpEntity<>("planning=5984&nextButton=Etape+suivante", headers);
-        resp = restTemplate.postForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/1", entity, String.class);
-        resp = restTemplate.getForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/2", String.class);
-        String body = resp.getBody();
-        System.out.println(body);
-        return !body.contains("plus de plage horaire");
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
---
+++ src/main/java/com/github/zg2pro/dispo/prefecture/extra/Comp.java
@@ -0,0 +1,123 @@
+package com.github.zg2pro.dispo.prefecture.extra;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author zg2pro
+ */
+@Component
+public class Comp {
+    
+    public boolean checkAvailibility() throws IOException {
+        //curl "http://www.val-de-marne.gouv.fr/booking/create/4963/1" 
+        //-H "Cookie: eZSESSID=9lp2b5rfmpn7eus77h094qu9b1; xtvrn=^$481980^$; xtan481980=-; xtant481980=1; cookies-accepte=oui" 
+        //-H "Origin: http://www.val-de-marne.gouv.fr" 
+        //-H "Accept-Encoding: gzip, deflate" 
+        //-H "Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6" 
+        //-H "Upgrade-Insecure-Requests: 1" 
+        //-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" 
+        //-H "Content-Type: application/x-www-form-urlencoded" 
+        //-H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" 
+        //-H "Cache-Control: max-age=0" 
+        //-H "Referer: http://www.val-de-marne.gouv.fr/booking/create/4963/1" 
+        //-H "Connection: keep-alive" 
+        //--data "planning=5985^&nextButton=Etape+suivante" --compressed
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().build());
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+        HttpHeaders headersPost = new HttpHeaders();
+        headersPost.set("Cookie", "eZSESSID=9lp2b5rfmpn7eus77h094qu9b1; xtvrn=^$481980^$; xtan481980=-; xtant481980=1; cookies-accepte=oui");
+        headersPost.set("Origin", "http://www.val-de-marne.gouv.fr");
+        headersPost.set("Accept-Encoding", "gzip, deflate");
+        headersPost.set("Accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6");
+        headersPost.set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+        headersPost.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+        headersPost.set("Upgrade-Insecure-Requests", "1");
+        headersPost.set("Content-Type", "application/x-www-form-urlencoded");
+        headersPost.set("Cache-Control", "max-age=0");
+        headersPost.set("Referer", "http://www.val-de-marne.gouv.fr/booking/create/4963/1");
+        headersPost.set("Connection", "keep-alive");
+
+        HttpEntity<String> entity = new HttpEntity<>("planning=5985^&nextButton=Etape+suivante", headersPost);
+
+        ResponseEntity<String> resPost = restTemplate.postForEntity("http://www.val-de-marne.gouv.fr/booking/create/4963/1", entity, String.class);
+
+        //curl "http://www.val-de-marne.gouv.fr/booking/create/4963/2" 
+        //-H "Accept-Encoding: gzip, deflate" 
+        //-H "Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6" 
+        //-H "Upgrade-Insecure-Requests: 1" 
+        //-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" 
+        //-H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" 
+        //-H "Referer: http://www.val-de-marne.gouv.fr/booking/create/4963/1" 
+        //-H "Cookie: eZSESSID=9lp2b5rfmpn7eus77h094qu9b1; xtvrn=^$481980^$; xtan481980=-; xtant481980=1; cookies-accepte=oui" 
+        //-H "Connection: keep-alive" -H "If-Modified-Since: Sun, 11 Feb 2018 18:54:59 GMT" -H "Cache-Control: max-age=0" 
+        //--compressed > test.html
+        HttpHeaders headersGet = new HttpHeaders();
+        headersGet.set("Accept-Encoding", "gzip, deflate");
+        headersGet.set("Accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6");
+        headersGet.set("Upgrade-Insecure-Requests", "1");
+        headersGet.set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+        headersGet.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+        headersGet.set("Referer", "http://www.val-de-marne.gouv.fr/booking/create/4963/1");
+        headersGet.set("Cookie", "eZSESSID=9lp2b5rfmpn7eus77h094qu9b1; xtvrn=^$481980^$; xtan481980=-; xtant481980=1; cookies-accepte=oui");
+        headersGet.set("Connection", "keep-alive");
+
+        ResponseEntity<String> resGet = restTemplate.exchange("http://www.val-de-marne.gouv.fr/booking/create/4963/2", HttpMethod.GET, entity, String.class);
+
+        String body = resGet.getBody();
+
+        boolean ret = !body.contains("plus de plage horaire");
+        return ret;
+    }
+
+    private boolean sendMail() throws IOException {
+        if (checkAvailibility()) {
+            //if (true) {
+            final NameValuePair[] data = {
+                new BasicNameValuePair("phone", "+33652942131"),
+                new BasicNameValuePair("message", "Hello world"),
+                new BasicNameValuePair("key", "textbelt")
+            };
+            HttpClient httpClient = HttpClients.createMinimal();
+            HttpPost httpPost = new HttpPost("https://textbelt.com/text");
+            httpPost.setEntity(new UrlEncodedFormEntity(Arrays.asList(data)));
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+
+            String responseString = EntityUtils.toString(httpResponse.getEntity());
+            System.out.println(responseString);
+        }
+        return true;
+    }
+    
+    
+    
+    @Scheduled(fixedRate = 10000 * 60)
+    public void sync() throws IOException{
+         System.out.println("###############");
+         sendMail();
+    }
+    
+    
+}
